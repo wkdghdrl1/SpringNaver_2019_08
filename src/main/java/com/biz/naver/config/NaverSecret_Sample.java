d9cf68b5b5e06b77fe93a80bb3038af1d3b384b8
wkdghdrl1 <power374@naver.com>
2019-08-07 11:28:44 +0900

    first

---
+++ src/main/java/com/biz/naver/config/NaverSecret_Sample.java
@@ -0,0 +1,14 @@
+package com.biz.naver.config;
+
+/*
+ *  네이버 오픈 API 사용 등록을 수행한 후
+ *  본인의 CLIENT ID와 CLIENT SECRET을 발급 받아
+ *  아래 String 변수에 값을 복사한 후
+ *  이 클래스를 NaverSecret 이름으로 변경한 후 프로젝트를
+ *  실행하시오.
+ */
+public class NaverSecret_Sample {
+	
+	public final static String NavarClientId ="YOUR NAVER CLIENT ID";
+	public final static String NaverClientSecret = "YOUR NAVER SECRET";
+}
